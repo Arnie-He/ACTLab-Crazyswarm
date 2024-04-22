4f1bb788c8f15d4766eef06b9615ffde0c8406fa
Arnie-He <arniehe2004@gmail.com>
2024-04-21 20:32:16 -0400

    synced the scripts

---
+++ ros_ws/src/crazyswarm/scripts/swap_position.py
@@ -0,0 +1,83 @@
+from pycrazyswarm import Crazyswarm
+import numpy as np
+import yaml
+
+def read_crazyflies_config(file_path):
+    with open(file_path, 'r') as file:
+        # Load the YAML file
+        config = yaml.safe_load(file)
+        crazyflies = config.get('crazyflies', [])
+        
+        # Extracting initial positions
+        initial_positions = {cf['id']: cf['initialPosition'] for cf in crazyflies}
+        return initial_positions
+
+# Path to your YAML file
+file_path = '../launch/crazyflies.yaml'
+initial_positions = read_crazyflies_config(file_path)
+print(initial_positions)
+
+
+TAKEOFF_DURATION = 5
+ACTION_GAP = 4
+HOVER_DURATION = 10
+NUM_COMMAND_REPEAT = 4
+TARGET_HEIGHT = 1
+
+NUM_CF = 2
+NUM_CF_TOTAL = 50
+
+def get_cf_id(cfs):
+    available_id = []
+    for id in range(1, NUM_CF_TOTAL):
+        try:
+            _ = cfs.crazyfliesById[id]
+        except:
+            continue
+        print("id")
+        available_id.append(id)
+    return available_id
+
+def main():
+    # Environment constant 
+    swarm = Crazyswarm()
+    timeHelper = swarm.timeHelper
+    cfs = swarm.allcfs
+    
+    available_cf_id = get_cf_id(cfs=cfs)
+
+    initial_positions1 = initial_positions[1]
+    initial_positions2 = initial_positions[2]
+    initial_positions1[2] = TARGET_HEIGHT
+    initial_positions2[2] = TARGET_HEIGHT
+
+    if(len(available_cf_id) != NUM_CF):
+        print(len(available_cf_id))
+        print("The number of cfs doesn't match")
+    else:
+        if(NUM_CF == 2):
+            cf0 = cfs.crazyfliesById[available_cf_id[0]]
+            cf1 = cfs.crazyfliesById[available_cf_id[1]]
+            cf0.takeoff(targetHeight=1.0, duration=TAKEOFF_DURATION)
+            cf1.takeoff(targetHeight=1.0, duration=TAKEOFF_DURATION)
+            timeHelper.sleep(5)
+            cf0.goTo([0.,0.,1.0], 0., 2., relative=False)
+            timeHelper.sleep(5)
+            cf1.goTo([0.,0.,0.8], 0., 2., relative=False)
+            timeHelper.sleep(15)
+            cf0.goTo(initial_positions1, 0., 2., relative=False)
+            cf1.goTo(initial_positions2, 0., 2., relative=False)
+            timeHelper.sleep(5)
+            cf0.goTo([0.,0.,0.8], 0., 2., relative=False)
+            cf1.goTo([0.,0.,1.0],0., 2., relative=False)
+            timeHelper.sleep(15)
+            cf0.goTo(initial_positions1, 0., 2., relative=False)
+            cf1.goTo(initial_positions2, 0., 2., relative=False)
+            timeHelper.sleep(5)
+            cfs.land(targetHeight=0.04, duration=TAKEOFF_DURATION)
+            timeHelper.sleep(TAKEOFF_DURATION + ACTION_GAP)
+            
+
+
+if __name__ == "__main__":
+    main()
